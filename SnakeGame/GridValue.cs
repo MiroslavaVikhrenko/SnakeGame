666c67749da9719bda1a8f7990aa56109dfbd036
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-11-16 14:52:21 -0700

    Add enum GridValue for positions in grid

---
+++ SnakeGame/GridValue.cs
@@ -0,0 +1,14 @@
+﻿
+namespace SnakeGame
+{
+    //enum for positions in the grid
+    public enum GridValue
+    {
+        Empty,
+        Snake,
+        Food,
+        Outside
+    }
+    //Outside => this value won't be stored in the grid array but it will be convenient in the code
+    //when the snakes tries to move outside the grid.
+}
