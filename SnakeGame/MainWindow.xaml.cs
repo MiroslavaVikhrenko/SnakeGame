9689b87e5a19d5e0976edb95dd930751715a4b45
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-11-16 18:00:18 -0700

    Add functionality to add grid images in MainWindow.xaml.cs files

--- SnakeGame/MainWindow.xaml.cs
+++ SnakeGame/MainWindow.xaml.cs
@@ -11,14 +11,60 @@ using System.Windows.Shapes;
 
 namespace SnakeGame
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    ///code behind
     public partial class MainWindow : Window
     {
+        //we need 2 variables for the number of rows and columns, we use 15 rows and 15 columns
+        private readonly int rows = 15, cols = 15;
+
+        //add a 2d image array for the image controls
+        //this array will make it easy to access the image for a given position in the grid
+        private readonly Image[,] gridImages;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            //Call SetupGrid() method and save the returned array and grid images
+            gridImages = SetupGrid();
+
+        }
+
+        //Method to set up grid
+        //it will add the required image controls to the game grid and return them in a 2d array for easy access
+        private Image[,] SetupGrid()
+        {
+            //we start by creating a 2d array 
+            Image[,] images = new Image[rows, cols];
+
+            //next we have to set up the number of rows and columns on the game grid
+            GameGrid.Rows = rows;
+            GameGrid.Columns = cols;
+
+            //then we loop over all grid positions 
+            for (int r = 0; r < rows; r++)
+            {
+                for (int c = 0; c < cols; c++)
+                {
+                    //for each of them we create a new image 
+                    Image image = new Image
+                    {
+                        //initially I want its source to be empty image asset
+                        Source = Images.Empty
+                    };
+
+                    //we store this image in the 2d array
+                    images[r, c] = image;
+
+                    //and add it as a child of the game grid
+                    GameGrid.Children.Add(image);
+                }
+            }
+
+            //outside the loops we return the images array
+            return images;
+
+            //in the constructor we call this method
         }
     }
 }
